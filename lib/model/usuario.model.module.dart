9f857c7d056a81bda8c98a5fcabf9b8a755675fc
Zeustk <112997729+Zeustk@users.noreply.github.com>
2025-03-12 15:00:49 -0500

    id

--- lib/model/usuario.model.module.dart
+++ lib/model/usuario.model.module.dart
@@ -1,6 +1,5 @@
 //model usuario
 class Usuario{
-  late String id;
   late String cedula;
   late String clave;
   late String cuenta;
@@ -9,7 +8,6 @@ class Usuario{
   Usuario({ required this.cedula, required this.clave, required this.cuenta,});
 
   Usuario.fromJson(Map<String, dynamic> json){
-    id = json['id'];
     cedula = json['nombre'];
     clave = json['clave'];
     cuenta = json['cuenta'];
